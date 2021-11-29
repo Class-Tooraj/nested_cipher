bea3954f5d1bcf0d8d5425c02c5820fa5f10b470
toorajjahangiri <toorajjahangiri@gmail.com>
2021-11-29 18:49:55 +0330

    Add Hashed Encode in Tests Some Changes

--- nested_cipher/test_b64.py
+++ nested_cipher/test_b64.py
@@ -1,7 +1,9 @@
-###########################################
-__author__ = "ToorajJahangiri"
-__email__ = "Toorajjahangiri@gmail.com"
-###########################################
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> #
+#           < IN THE NAME OF GOD >           #
+# ------------------------------------------ #
+__AUTHOR__ = "ToorajJahangiri"
+__EMAIL__ = "Toorajjahangiri@gmail.com"
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
 
 # IMPORTS TIME & TYPE
 import time
@@ -22,7 +24,7 @@ from b64 import rlb64_encode, rlb64_decode
 class Test_b64:
 
     def __init__(self) -> None:
-        self.__result: NamedTuple = NamedTuple('result', (('passed', bool), ('name', str), ('encoded', bytes), ('decoded', bytes), ('time', str)))
+        self.__result: NamedTuple = NamedTuple('result', (('passed', bool), ('name', str), ('encoded', bytes), ('decoded', bytes), ('time', str), ('hashed', bytes)))
         self.__ret: NamedTuple = NamedTuple('test', (('data', bytes), ('result', NamedTuple)))
         self.__all_test: list[Callable] = [
             self._ab64,
@@ -49,6 +51,7 @@ class Test_b64:
         t2 = time.perf_counter()
         dec = ab64_decode(enc)
         t3 = time.perf_counter()
+        hashed = hash(enc)
 
         times = f"en_code: <{t2 - t1:.4}>de_code: <{t3 - t2:.4}>"
 
@@ -58,7 +61,7 @@ class Test_b64:
             print(AssertionError(f'<TEST::{name}::FALSE>'))
             passed = False
 
-        return self.__result(passed, name, enc, dec, times)
+        return self.__result(passed, name, enc, dec, times, hashed)
 
     def _mb64(self, data: bytes) -> bool:
         name = 'mb64'
@@ -67,6 +70,7 @@ class Test_b64:
         t2 = time.perf_counter()
         dec = mb64_decode(enc)
         t3 = time.perf_counter()
+        hashed = hash(enc)
 
         times = f"en_code: <{t2 - t1:.4}>de_code: <{t3 - t2:.4}>"
 
@@ -76,7 +80,7 @@ class Test_b64:
             print(AssertionError(f'<TEST::{name}::FALSE>'))
             passed = False
 
-        return self.__result(passed, name, enc, dec, times)
+        return self.__result(passed, name, enc, dec, times, hashed)
 
     def _rb64(self, data: bytes) -> bool:
         name = 'rb64'
@@ -85,6 +89,7 @@ class Test_b64:
         t2 = time.perf_counter()
         dec = rb64_decode(enc)
         t3 = time.perf_counter()
+        hashed = hash(enc)
 
         times = f"en_code: <{t2 - t1:.4}>de_code: <{t3 - t2:.4}>"
 
@@ -94,7 +99,7 @@ class Test_b64:
             print(AssertionError(f'<TEST::{name}::FALSE>'))
             passed = False
 
-        return self.__result(passed, name, enc, dec, times)
+        return self.__result(passed, name, enc, dec, times, hashed)
 
     def _eb64(self, data: bytes) -> bool:
         name = 'eb64'
@@ -103,6 +108,7 @@ class Test_b64:
         t2 = time.perf_counter()
         dec = eb64_decode(enc)
         t3 = time.perf_counter()
+        hashed = hash(enc)
 
         times = f"en_code: <{t2 - t1:.4}>de_code: <{t3 - t2:.4}>"
 
@@ -112,7 +118,7 @@ class Test_b64:
             print(AssertionError(f'<TEST::{name}::FALSE>'))
             passed = False
 
-        return self.__result(passed, name, enc, dec, times)
+        return self.__result(passed, name, enc, dec, times, hashed)
 
     def _lb64(self, data: bytes) -> bool:
         name = 'lb64'
@@ -121,6 +127,7 @@ class Test_b64:
         t2 = time.perf_counter()
         dec = lb64_decode(enc)
         t3 = time.perf_counter()
+        hashed = hash(enc)
 
         times = f"en_code: <{t2 - t1:.4}>de_code: <{t3 - t2:.4}>"
 
@@ -130,7 +137,7 @@ class Test_b64:
             print(AssertionError(f'<TEST::{name}::FALSE>'))
             passed = False
 
-        return self.__result(passed, name, enc, dec, times)
+        return self.__result(passed, name, enc, dec, times, hashed)
 
     def _rab64(self, data: bytes) -> bool:
         name = 'rab64'
@@ -139,6 +146,7 @@ class Test_b64:
         t2 = time.perf_counter()
         dec = rab64_decode(enc)
         t3 = time.perf_counter()
+        hashed = hash(enc)
 
         times = f"en_code: <{t2 - t1:.4}>de_code: <{t3 - t2:.4}>"
 
@@ -148,7 +156,7 @@ class Test_b64:
             print(AssertionError(f'<TEST::{name}::FALSE>'))
             passed = False
 
-        return self.__result(passed, name, enc, dec, times)
+        return self.__result(passed, name, enc, dec, times, hashed)
 
     def _rmb64(self, data: bytes) -> bool:
         name = 'rmb64'
@@ -157,6 +165,7 @@ class Test_b64:
         t2 = time.perf_counter()
         dec = rmb64_decode(enc)
         t3 = time.perf_counter()
+        hashed = hash(enc)
 
         times = f"en_code: <{t2 - t1:.4}>de_code: <{t3 - t2:.4}>"
 
@@ -166,7 +175,7 @@ class Test_b64:
             print(AssertionError(f'<TEST::{name}::FALSE>'))
             passed = False
 
-        return self.__result(passed, name, enc, dec, times)
+        return self.__result(passed, name, enc, dec, times, hashed)
 
     def _reb64(self, data: bytes) -> bool:
         name = 'reb64'
@@ -175,6 +184,7 @@ class Test_b64:
         t2 = time.perf_counter()
         dec = reb64_decode(enc)
         t3 = time.perf_counter()
+        hashed = hash(enc)
 
         times = f"en_code: <{t2 - t1:.4}>de_code: <{t3 - t2:.4}>"
 
@@ -184,7 +194,7 @@ class Test_b64:
             print(AssertionError(f'<TEST::{name}::FALSE>'))
             passed = False
 
-        return self.__result(passed, name, enc, dec, times)
+        return self.__result(passed, name, enc, dec, times, hashed)
 
     def _rlb64(self, data: bytes) -> bool:
         name = 'rlb64'
@@ -193,6 +203,7 @@ class Test_b64:
         t2 = time.perf_counter()
         dec = rlb64_decode(enc)
         t3 = time.perf_counter()
+        hashed = hash(enc)
 
         times = f"en_code: <{t2 - t1:.4}>de_code: <{t3 - t2:.4}>"
 
@@ -202,23 +213,40 @@ class Test_b64:
             print(AssertionError(f'<TEST::{name}::FALSE>'))
             passed = False
 
-        return self.__result(passed, name, enc, dec, times)
+        return self.__result(passed, name, enc, dec, times, hashed)
 
 if __name__ == '__main__':
     import os.path as _path
     import json
-    result_file = _path.realpath('test_file/test_nested_b64.jsn')
 
+    # PATH JSON RESULTS DATA
+    RESULTS_PATH = './test/test_nested_b64.json'
+
+    RESULT_FILE_VALID_PATH = _path.realpath(RESULTS_PATH)
+
+    # `FALSE` MEAN ENCODED DATA NOT IN RESULTS FILE IF NEED ENCODED DATA CHANGE TO `TRUE`
+    ENCODE_DATA_ADD_RESULTS = False
+
+    # TEST DATA STRING & BYTES
     test_data = [b'T3ST!N!T', 'T3ST_STR!NG', 'H3LL0_H3LL0', b'data_DATA', b'5236989566321', b'0x45']
 
+    # ACTIVE TEST CLASS
     tester = Test_b64()
-    with open(result_file, 'w') as f:
+    
+    # TEST ACTION & RESULTS PUT IN THE JSON FILE
+    # ALL TESTS STRING IS PROPERTY NAME & IN TO METHOD IS PROPERTY NAME
+    with open(RESULT_FILE_VALID_PATH, 'w') as f:
         res_js = {}
         res_js['TEST_START'] = {'TIME':time.strftime('%X%x'), 'TEST_DATA': str(test_data)}
         counter = 0
         for it in test_data:
             t = tester.test(it)
-            val_res = ({'NAME':str(j.name),'PASSED':str(j.passed),'TIME':str(j.time),'ENCODED':j.encoded.decode('ascii')} for j in t[1])
+
+            if ENCODE_DATA_ADD_RESULTS:
+                val_res = ({'NAME':str(j.name),'PASSED':str(j.passed),'TIME':str(j.time),'ENC_HASH': str(j.hashed), 'ENCODED':j.encoded.decode('ascii')} for j in t[1])
+            else:
+                val_res = ({'NAME':str(j.name),'PASSED':str(j.passed),'TIME':str(j.time),'ENC_HASH': str(j.hashed)} for j in t[1])
+
             res_js[str(t[0])] = {i['NAME']: i for i in val_res}
             _ex = '\n\t'.join([f'<{i.name}> -> <{i.passed}> [{i.time}]' for i in t[1]])
             print(f'TEST [{counter}] [{t[0]}] :: \n\t{_ex}')
